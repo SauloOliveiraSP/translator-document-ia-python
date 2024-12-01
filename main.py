10277d4e6f352397c08a988ee8318412b3e1785b
Saulo de Oliveira <Sauloliveira.SP@gmail.com>
2024-12-01 17:09:43 -0300

    adicionando comentarios

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import requests
 from docx import Document
 import os
 
-subscription_key = ""
+# Configurações do Serviço Azure Translator
+subscription_key = "sua_subscription_key"
 endpoint = 'https://api.cognitive.microsofttranslator.com'
 location = "eastus2"
-language_destination = 'pt-br'
+language_destination = 'pt-br'  # Idioma desejado para tradução
 
+# Função para traduzir texto
 def translator_text(text, target_language):
     path = '/translate'
-
     constructed_url = endpoint + path
     headers = {
         'Ocp-Apim-Subscription-Key': subscription_key,
@@ -17,21 +18,13 @@ def translator_text(text, target_language):
         'Content-type': 'application/json',
         'x-ClientTraceId': str(os.urandom(16))
     }
-
-    body = [{
-        'text': text
-    }]
-    
-    params = {
-        'api-version': "3.0",
-        'from': 'en',
-        'to': target_language
-    }
-
+    body = [{'text': text}]
+    params = {'api-version': "3.0", 'from': 'en', 'to': target_language}
     request = requests.post(constructed_url, params=params, headers=headers, json=body)
     response = request.json()
-    return response[0]("translations")[0]("text")
+    return response[0]["translations"][0]["text"]
 
+# Função para traduzir documentos .docx
 def traslate_document(path):
     document = Document(path)
     full_text = []
@@ -48,8 +41,7 @@ def traslate_document(path):
     translated_doc.save(path_translated)
     return path_translated
 
-
-translator_text("Hello Word!", language_destination)
-
-input_file = "/content/musica.docx"
-traslate_document(input_file)
+# Exemplo de Uso
+translator_text("Hello World!", language_destination)  # Traduz texto simples
+input_file = "musica.docx"  # Substitua pelo caminho do arquivo .docx
+traslate_document(input_file)  # Traduz o documento
